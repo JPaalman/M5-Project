9fe5774cd2e77c1b99eed168322dd039f16c473a
Jaïr Paalman <j.paalman@outlook.com>
2018-10-26 12:34:47 +0200

    Made player functional

--- game/player.py
+++ game/player.py
@@ -1,42 +1,52 @@
 import pygame as pg
 
+from game.settings import *
+
 
 class Player(pg.sprite.Sprite):
+    """
+    I'm a docstring hurr durr
+    """
+
     ACCELERATION = 60 / FPS  # dummy value
-    FRICTION = 0.5  # dummy value
+    FRICTION = 1  # dummy value
     GRAVITY = 1  # dummy value
     LIFE = 5
 
     def __init__(self, x, y, h, w):
-        super.__init__()
+        super().__init__()
 
         self.x = x  # x position
         self.y = y  # y position
         self.h = h  # height
         self.w = w  # width
 
-        self.vel = pg.math.Vector2(0, 0)
-        self.acc = pg.math.Vector2(0, GRAVITY)
+        self.vel = pg.Vector2(0, 0)
+        self.acc = pg.Vector2(0, self.GRAVITY)
 
         self.rect = pg.Rect(x, y, w, h)
 
         self.image = pg.Surface(w, h)
         self.image.fill(RED)
-        # self.image = pg.image.load()
+        # self.image = pg.image.load("../resources/player.*")
 
     def update(self):
+        """
+        I'm also a docstring hurr durr
+        """
+
         # key updates
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
             self.acc.x -= self.a
-        if keys[pygame.K_RIGHT]:
+        if keys[pg.K_RIGHT]:
             self.acc.x += self.a
         # test jumping function
-        if keys[pygame.K_SPACE]:
+        if keys[pg.K_SPACE]:
             self.vel.y = -20
 
         # friction
-        self.acc.x += self.vel.x * FRICTION
+        self.acc.x -= self.vel.x * self.FRICTION
 
         # update vel and pos
         self.vel.x += self.acc.x
@@ -45,12 +55,6 @@ class Player(pg.sprite.Sprite):
         self.vel.y += self.acc.y
         self.y += self.vel.y
 
-        # handle collision
-        self.handleCollision()
-
-    def handleCollision(self):
-        if self.rect.colliderect(enemy.rect):
-            while self.rect.colliderect(tile.rect):
-                self.pos.y += 1;
-            self.acc.y = 0;
-            self.vel.x = 0;
+        # dummy collision
+        if self.y > 700:
+            self.y = 700
