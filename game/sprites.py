e013adb065555111e04956145fc376e249c2f75e
Annefleur Uitzetter <annefleuruitzetter@hotmail.com>
2018-10-29 14:51:00 +0100

    lhg

--- game/sprites.py
+++ game/sprites.py
@@ -46,7 +46,7 @@ class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((w, h))
-        self.image.fill(GREEN)
+        self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
