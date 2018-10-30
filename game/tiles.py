d1f03f3be10ca670b81e48bc3390e809e40816ed
Jaïr Paalman <j.paalman@outlook.com>
2018-10-30 10:59:39 +0100

    Removed redundant code

--- game/tiles.py
+++ game/tiles.py
@@ -1,10 +1,8 @@
 import pygame as pg
 
-from game.settings import TILESIZE
-
 
 class Tile(pg.sprite.Sprite):
-    '''
+    """
     instance variables:
     x (x-coordinate top left of tile)
     y (y-coordinate top left of tile)
@@ -12,18 +10,22 @@ class Tile(pg.sprite.Sprite):
     h (height
     texturePath (path to texture)
     rect (hitbox for collision)
-    '''
 
-    def __init__(self, new_x, new_y, new_byte, new_data):
+    bytemap:
+    32 = nothing
+     = tile
+
+    """
+
+    def __init__(self, x, y, new_byte, new_data):
         super().__init__()
-        self.x = new_x
-        self.y = new_y
+
+        self.x = x
+        self.y = y
         self.byte = new_byte
         self.data = new_data
-        if self.data != 0:
-            self.byte = 64
+
         # self.texturePath = self.setTexture()
-        self.rect = pg.Rect(self.x, self.y, TILESIZE, TILESIZE)
 
     def setTexture(self):
         return ""
