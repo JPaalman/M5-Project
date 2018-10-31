f5f597294262419b9d13023a7c9efb6d9e0b528a
NaN <o.r.lijzenga@student.utwente.nl>
2018-10-31 12:49:03 +0100

    Woopsie

--- game/map/colorMap.py
+++ game/map/colorMap.py
@@ -1,16 +1,22 @@
 # coding=utf-8
 # Definitions of all colors
+
+# Standard colors
 BLACK       = (0,   0,   0  )
 BROWN       = (153, 76,  0  )
 GREEN       = (0,   255, 0  )
 BLUE        = (0,   0,   255)
 RED         = (255, 0,   0  )
+WHITE       = (255, 255, 255)
+
+# Game colors
 STDPLATFORM = (53,  72,  104)
 GRASS       = (48,  150, 51 )
 DEATHTILE   = (237, 109, 30 )
 FLOORFILLER = (124, 76,  28 )
 FINISH      = (255, 215, 0  )
-WHITE       = (0,   0,   0  )
+
+# Invisible tiles
 MAPBORDER   = None
 PLAYERSPAWN = None
 I_DEATHTILE = None
