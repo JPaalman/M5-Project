a7915bee04ce00781890544dd0f83864423e20c7
NaN <o.r.lijzenga@student.utwente.nl>
2018-11-02 12:58:00 +0100

    Started working on main menu

--- Test.py
+++ Test.py
@@ -1,3 +1,12 @@
-import map.mapTool
+from game.menu import Menu
+import pygame as pg
+from game import settings
+import time
 
-map.mapTool
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((settings.WIDTH, settings.HEIGHT))  # (0, pg.FULLSCREEN)[FULLSCREEN]
+pg.display.set_caption(settings.TITLE)
+
+Menu(screen)
+time.sleep(5)
