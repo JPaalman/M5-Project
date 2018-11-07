1bf9d44643bb656cd5e8e932d74cffb26683c145
NaN <o.r.lijzenga@student.utwente.nl>
2018-11-07 10:57:48 +0100

    Pi stuff

--- game/spiconroller.py
+++ game/spiconroller.py
@@ -35,8 +35,8 @@ class SPIController:
             start = time.time()
 
             print(str(self.run))
-            self.data[0] = wiringpi.wiringPiSPIDataRW(SPIchannel, to_send)[1]
-            self.data[1] = wiringpi.wiringPiSPIDataRW(SPIchannel, to_send)[1]
+            self.data[0] = int(wiringpi.wiringPiSPIDataRW(SPIchannel, to_send)[1])
+            self.data[1] = int(wiringpi.wiringPiSPIDataRW(SPIchannel, to_send)[1])
             print("sent:")
             print(value)
             # value += 1
